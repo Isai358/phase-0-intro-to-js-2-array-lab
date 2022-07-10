935dc23068819e519e6d36cbef85c0c7d23973ef
Isai <castanedaisai303@gmail.com>
2022-07-10 16:12:16 -0700

    commit tree 5df9f313eefe7de4eb321dada08883a3368c4399

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield',];
+
+function destructivelyAppendCat(name){
+    cats.push('Ralph')
+    return cats
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+    return cats
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+}
+function appendCat(name){
+    const newArray = cats.slice();
+    newArray.push(name)
+    return newArray
+}
+function prependCat(name){
+    const newArray = cats.slice();
+    newArray.unshift(name)
+    return newArray
+}
+function removeLastCat(){
+    const newArray = cats.slice();
+    newArray.pop()
+    return newArray
+}
+function removeFirstCat(){
+    const newArray = cats.slice();
+    newArray.shift()
+    return newArray
+}
